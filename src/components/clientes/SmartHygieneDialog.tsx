549752f7c726e9566b5072e34dae9a36315b4817
Marcio Gabriel Holm Monteiro <marciogholmm@gmail.com>
2026-05-13 12:21:02 -0300

    fix: restore missing ZapIcon component

--- src/components/clientes/SmartHygieneDialog.tsx
+++ src/components/clientes/SmartHygieneDialog.tsx
@@ -649,3 +649,9 @@ function formatWithHighlight(text: string, range?: [number, number]) {
     </>
   );
 }
+
+function ZapIcon(props: any) {
+  return (
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2" /></svg>
+  );
+}
